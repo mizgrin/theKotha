89fbac60bd80b4271f3c3672975cc1eec4ed3b1b
mijan123 <msmizans@gmail.com>
2024-05-15 13:47:32 +0545

    details pages

--- js/custom.js
+++ js/custom.js
@@ -63,3 +63,38 @@ $(this).toggleClass('reverse');
   $('.filter-wrap').slideToggle(300);
   $('body').toggleClass('overflow-hidden');
 })
+
+$('.room-picture-slider').slick({
+  infinite:true,
+  arrows: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
